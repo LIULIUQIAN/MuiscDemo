eab2e7c7ce5dbad80dca64ecd165f3381dca97f5
3284904004@qq.com <FFzf2016>
2019-11-23 18:59:02 +0800

    登录页布局

--- app/src/main/java/com/example/muiscdemo/activity/LoginActivity.java
+++ app/src/main/java/com/example/muiscdemo/activity/LoginActivity.java
@@ -4,13 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.muiscdemo.MainActivity;
 import com.example.muiscdemo.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.OnClick;
+
+public class LoginActivity extends BaseCommonActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @OnClick(R.id.bt_login)
+    public void onLogin() {
+        startActivity(LoginPhoneActivity.class);
+    }
+
+    @OnClick(R.id.bt_register)
+    public void onRegister() {
+        startActivity(RegisterActivity.class);
+    }
+
+    @OnClick(R.id.tv_enter)
+    public void onEnter() {
+        startActivityAfterFinishThis(MainActivity.class);
+    }
+
+    @OnClick(R.id.iv_login_qq)
+    public void onLoginQQ() {
+
     }
 }
